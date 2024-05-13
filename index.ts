8929c883060fba969467968ec67496b63b0c0c7d
Areeba Bano <161935706+areebabano@users.noreply.github.com>
2024-05-14 01:00:41 +0500

    this is cli based oop-bank

---
+++ index.ts
@@ -0,0 +1,195 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { faker, th } from "@faker-js/faker";
+
+// OOP_MYBANK 
+// create class for customer
+class Customer {
+  firstName: string;
+  lastName: string;
+  age: number;
+  gender: string;
+  mobileNo: number;
+  accountNo: number;
+  constructor(
+    fName: string,
+    lName: string,
+    age: number,
+    gender: string,
+    mobNo: number,
+    accNo: number
+  ) {
+    this.firstName = fName;
+    this.lastName = lName;
+    this.age = age;
+    this.gender = gender;
+    this.mobileNo = mobNo;
+    this.accountNo = accNo;
+  }
+}
+
+// create interface for bank account
+
+interface BankAccount {
+  accountNo: number;
+  balance: number;
+}
+
+// create class for bank
+
+class Bank {
+  customer: Customer[] = [];
+  account: BankAccount[] = [];
+  customerAdd(obj: Customer) {
+    this.customer.push(obj);
+  }
+  accountNoAdd(obj: BankAccount) {
+    this.account.push(obj);
+  }
+  updateTransaction(accountObj: BankAccount) {
+    let updateAcc = this.account.filter(
+      (acc) => acc.accountNo !== accountObj.accountNo
+    );
+    this.account = [...updateAcc, accountObj];
+  }
+}
+console.log(
+  chalk.bold.yellow(
+    ">>>>>>>>>>>>>>>>>-------------------------------<<<<<<<<<<<<<<<<"
+  )
+);
+console.log(
+  chalk.bold.cyanBright(
+    "\n~~~~~~~~~~~***********| WELCOME TO MY BANK |***********~~~~~~~~~~~~\n"
+  )
+);
+console.log(
+  chalk.bold.yellow(
+    ">>>>>>>>>>>>>>>>>-------------------------------<<<<<<<<<<<<<<<<"
+  )
+);
+
+let myBank = new Bank();
+
+// create customer
+// create for loop
+
+for (let i: number = 1; i <= 4; i++) {
+  let fName = faker.person.firstName("male");
+  let lName = faker.person.lastName();
+  let mobNo = parseInt(faker.string.numeric("3##########"));
+  const cus = new Customer(fName, lName, 18 * i, "male", mobNo, 4000 + i);
+  myBank.customerAdd(cus);
+  myBank.accountNoAdd({ accountNo: cus.accountNo, balance: 1000 * i });
+}
+
+// create bank functionallity
+
+async function bankService(bank: Bank) {
+  do {
+    let services = await inquirer.prompt({
+      name: "select",
+      type: "list",
+      message: chalk.bold.cyan("\nPlease select the service?\n"),
+      choices: [
+        chalk.bold.green("View Balance"),
+        chalk.bold.grey("Cash Withdraw"),
+        chalk.bold.magenta("Cash Deposit"),
+        chalk.bold.red("Exit"),
+      ],
+    });
+    // condition for view balance
+    if (services.select == chalk.bold.green("View Balance")) {
+      let response = await inquirer.prompt({
+        name: "AccNo",
+        type: "input",
+        message: chalk.bold.yellow("\nPlease enter your Account No:"),
+      });
+      let account = myBank.account.find(
+        (acc) => acc.accountNo == response.AccNo
+      );
+      if (!account) {
+        console.log(chalk.red.bold("\nInvalid Account No!\n"));
+      }
+      if (account) {
+        let name = myBank.customer.find(
+          (item) => item.accountNo == account.accountNo
+        );
+        console.log(
+          chalk.bold.cyan(`\nDear ${chalk.green.italic(name?.firstName)} ${chalk.green.italic(
+            name?.lastName
+          )} your Account Balance is ${chalk.bold.green(
+            `$${account.balance}`
+          )}\n`)
+        );
+      }
+    }
+    // condition for cash withdraw
+    if (services.select == chalk.bold.gray("Cash Withdraw")) {
+      let response = await inquirer.prompt({
+        name: "AccNo",
+        type: "input",
+        message: chalk.bold.yellow("\nPlease enter your Account No:"),
+      });
+      let account = myBank.account.find(
+        (acc) => acc.accountNo == response.AccNo
+      );
+      if (!account) {
+        console.log(chalk.red.bold("\nInvalid Account No!\n"));
+      }
+      if (account) {
+        let answer = await inquirer.prompt({
+          name: "dollar",
+          type: "number",
+          message: chalk.bold.green("\nPlease enter your Amount"),
+        });
+        if (answer.dollar > account.balance) {
+          console.log(chalk.bold.red("\nInsufficient Balance!\n"));
+        } else {
+          console.log(chalk.bold.cyan(`\nYour Cash "$${chalk.bold.green(answer.dollar)}" Withdraw Successfully...\n`));
+        }
+        let newBalance = account.balance - answer.dollar;
+        // update transaction method call
+        bank.updateTransaction({
+          accountNo: account.accountNo,
+          balance: newBalance,
+        });
+      }
+    }
+    // condition for cash deposit
+    if (services.select == chalk.bold.magenta("Cash Deposit")) {
+      let response = await inquirer.prompt({
+        name: "AccNo",
+        type: "input",
+        message: chalk.bold.yellow("\nPlease enter your Account No:"),
+      });
+      let account = myBank.account.find(
+        (acc) => acc.accountNo == response.AccNo
+      );
+      if (!account) {
+        console.log(chalk.red.bold("\nInvalid Account No!\n"));
+      }
+      if (account) {
+        let answer = await inquirer.prompt({
+          name: "dollar",
+          type: "number",
+          message: chalk.bold.green("\nPlease enter your Amount"),
+        });
+        let newBalance = account.balance + answer.dollar;
+        // update transaction method call
+        bank.updateTransaction({
+          accountNo: account.accountNo,
+          balance: newBalance,
+        });
+        console.log(chalk.bold.cyan(`\nYour Cash "$${chalk.bold.green(answer.dollar)}" Deposit Successfully...\n`));
+      }
+    }
+    if (services.select == chalk.bold.red("Exit")) {
+        console.log(chalk.bold.green("\n>>>>>>>--------ALWAYS CHOOSE MY BANK--------<<<<<<<\n"));
+      process.exit();
+    }
+  } while (true);
+}
+
+bankService(myBank);
